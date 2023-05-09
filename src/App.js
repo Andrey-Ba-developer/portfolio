75b612b483c4ae49a7ec13e053cd98cf7fdfd3e0
Andrey-Ba-developer <and.balyaeff@yandex.ru>
2023-05-09 13:26:14 +0700

    feat: installing and getting started with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./styles/main.css";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
@@ -11,15 +13,21 @@ function App() {
 	return (
 		<div className="App">
 
-			<Navbar />
-			{/* <Home /> */}
-			{/* <Projects/> */}
-			<Project/>
-			{/* <Contacts/> */}
-			<Footer />
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/projects" element={<Projects />} />
+					<Route path="/project" element={<Project />} />
+					<Route path="/contacts" element={<Contacts />} />
+				</Routes>
+				<Footer />
+			</Router>
 
 
-		</div>
+
+
+		</div >
 	);
 }
 
