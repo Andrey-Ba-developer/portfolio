c8fe11e728885c57d775004e79c6563b587112af
Andrey-Ba-developer <and.balyaeff@yandex.ru>
2023-05-10 12:38:27 +0700

    feat: switching dark and light themes using hooks

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,7 @@
-import sun from "./../../img/icons/sun.svg";
-import moon from "./../../img/icons/moon.svg";
-
 import { NavLink } from "react-router-dom";
+import BtnDarkMod from "../btnDarkMode/BtnDarkMod";
+import "./style.css";
 
-import "./style.css"
 
 
 const Navbar = () => {
@@ -20,11 +18,7 @@ const normalLink = 'nav-list__link';
 						<strong>Freelancer</strong>
 					</NavLink>
 
-
-					<button className="dark-mode-btn">
-						<img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-						<img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-					</button>
+					<BtnDarkMod/>
 
 					<ul className="nav-list">
 						<li className="nav-list__item">
