6328960043bc75abd05a7276a3c8a67986782438
Andrey-Ba-developer <and.balyaeff@yandex.ru>
2023-06-02 14:40:36 +0700

    feat: added a new project xiomi store

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -7,31 +7,42 @@ import project02Big from "./../img/projects/weather-big.jpg"
 import project03 from "./../img/projects/shelter.jpg"
 import project03Big from "./../img/projects/shelter-big.jpg"
 
+import project04 from "./../img/projects/Xiaomi-Shop.jpg"
+import project04Big from "./../img/projects/Xiaomi-Shop-big.jpg"
+
 const projects = [
 	{
 		title: 'Сar service',
-		skills: 'HTML, CSS, Responsive design',
+		skills: 'HTML5, CSS, Responsive design',
 		img: project01,
 		imgBig: project01Big,
 		gitHubLink: 'https://github.com',
 		link: 'https://andrey-ba-developer.github.io/09.01.23-Cars-final/#en',
 	},
 	{
-		title: 'site weather',
-		skills: 'HTML, CSS, Responsive design, JavaScript, working with API, Git',
+		title: 'Weather app',
+		skills: 'HTML5, CSS, Responsive design, JavaScript, working with API, Git',
 		img: project02,
 		imgBig: project02Big,
 		gitHubLink: 'https://github.com',
 		link: 'https://andrey-ba-developer.github.io/siteWether/',
 	},
 	{
-		title: 'site shelter',
-		skills: 'HTML, CSS, Responsive design, JavaScript, Git',
+		title: 'Shelter website',
+		skills: 'HTML5, CSS, Responsive design, JavaScript, Git',
 		img: project03,
 		imgBig: project03Big,
 		gitHubLink: 'https://github.com',
 		link: 'https://rolling-scopes-school.github.io/andrey-ba-developer-JSFE2023Q1/shelter/pages/main/index.html',
-	}
+	},
+	{
+		title: 'Xiaomi-Shop',
+		skills: 'HTML5, CSS, Responsive design, JavaScript, React, Material UI, LocalStorage',
+		img: project04,
+		imgBig: project04Big,
+		gitHubLink: 'https://github.com',
+		link: 'https://6473539c56504435d7bef459--comfy-cactus-2cf054.netlify.app/',
+	},
 ];
 
 export { projects }
